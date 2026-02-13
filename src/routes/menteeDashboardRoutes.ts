6e3fc2b795de8ff176f0a08e633077bbcabb1f39
Kingsley-codes <agbamkingsley@gmail.com>
2026-02-13 15:52:54 +0100

    submit assignment ready

--- src/routes/menteeDashboardRoutes.ts
+++ src/routes/menteeDashboardRoutes.ts
@@ -3,11 +3,17 @@ import { userAuthenticate } from "../middleware/authenticationMiddleware.js";
 import {
   fetchMyAssignments,
   fetchMySessions,
+  makeSubmission,
 } from "../controllers/menteeDashboardControllers.js";
 
 const menteeDashboardRouter = express.Router();
 
 menteeDashboardRouter.get("/assignments", userAuthenticate, fetchMyAssignments);
+menteeDashboardRouter.post(
+  "/assignments/submit",
+  userAuthenticate,
+  makeSubmission,
+);
 menteeDashboardRouter.get("/sessions", userAuthenticate, fetchMySessions);
 
 export default menteeDashboardRouter;
